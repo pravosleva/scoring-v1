49059aa98ae7b01fbf56ffdec7b53572775aa3c1
Dan <selection4test@gmail.com>
2022-10-27 04:39:41 +0300

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Package TypeScript npm starter Changelog
+
+## 0.0.0 (2020-02-02)
+
+### New Feature
+
+- added `sum` function
---
+++ __tests__/index.test.ts
@@ -0,0 +1,15 @@
+import sum from '../src';
+
+describe('sum', () => {
+  it.each([
+    [2, 3, 5],
+    [6, 4, 10],
+    [3, 3, 6],
+    [0, 0, 0],
+    [522, 11, 533],
+  ])('should add two numbers: %d, %d', (a, b, expected) => {
+    const result = sum(a, b);
+
+    expect(result).toBe(expected);
+  });
+});
